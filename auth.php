a31f97547372e1941843a53d2c3a151e1697957a
amprogramz <amcdaugale@student.cccs.edu>
2019-11-30 10:23:49 -0700

    adding updated code. this is auto generated message.

--- auth.php
+++ auth.php
@@ -4,8 +4,8 @@
     $user_password = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $user_email = $_POST('email');
-        $user_password = $_POST('password');
+        $user_email = $_POST["email_addr"];
+        $user_password = $_POST["pass"];
     }
 
     echo $user_email;
